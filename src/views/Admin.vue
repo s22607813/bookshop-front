02d3ee1655e62ebcdf202cb9b3d19354bbeb44d8
s22607813 <80014615+s22607813@users.noreply.github.com>
2021-08-26 03:12:10 +0800

    add admin.vue page transition
    add admin.vue page transition

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -7,7 +7,8 @@ b-container(fluid)#admin.page
       b-btn.btn(to='/admin/admindonatelist' exact exact-active-class="active" variant="" size="lg").mx-2.mt-md-3.px-0 審 核 清 單
       b-btn.btn(to='/admin/adminproducts' exact exact-active-class="active" variant="" size="lg").mx-2.mt-md-3.px-0 書 籍 管 理
   b-row.down.offset-md-2
-    router-view
+    vue-page-transition(name="fade-in-left").w-100
+      router-view
 </template>
 
 <script>
