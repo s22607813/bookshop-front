bec0e9c295fdc651d372c90effde1a82c67a208c
s22607813 <80014615+s22607813@users.noreply.github.com>
2021-08-26 02:04:45 +0800

    change book.vue
    change book.vue

--- src/views/Book.vue
+++ src/views/Book.vue
@@ -1,37 +1,34 @@
 <template lang="pug">
 b-container(fluid)#book.text-dark.px-5
-  b-overlay(:show="!checked")
-    b-row
-      b-col
-        h3.text-left.mt-4
-          span(v-if="this.genre === 1").font-weight-bold.title 文學類別
-          span(v-else-if="this.genre === 2").font-weight-bold.title 雜誌類別
-          span(v-else-if="this.genre === 3").font-weight-bold.title 教科書類別
-          span(v-else).font-weight-bold.title 其他
-    hr
-    b-row(align-v="stretch").mt-5
-      b-col.book_img(cols="12" md="4").mb-4.pr-md-2.pr-lg-1
-        img.w-100(:src="image")
-      b-col(cols="12"  md="8").pl-lg-5.pl-md-4
-        h1.text-left.mt-4.mb-4
-          span(style="color: #495580").font-weight-bold {{ name }}
-        h2.text-left.mb-4
-          span(style="color: #495580").font-weight-bold 捐贈者：
-        h3.text-left.mb-4
-          span {{ from.name }}
-        h2.text-left.mb-4
-          span(style="color: #495580").font-weight-bold 說明：
-        h4.text-left.mb-4
-          textarea(disabled).w-100.p-2 {{ description }}
-    hr
-    b-row
-      b-col(cols="12").mb-4.d-flex.justify-content-center
-        b-button(href='#' variant='success' @click="addCart" size="lg" v-if="this.$store.state.user.role !== 1").w-50.mt-3
-          #text_cart
-          b-icon(icon="bag-check-fill")
-          |  放入購物車
-    template(#overlay)
-      h1 審核中
+  b-row
+    b-col
+      h3.text-left.mt-4
+        span(v-if="this.genre === 1").font-weight-bold.title 文學類別
+        span(v-else-if="this.genre === 2").font-weight-bold.title 雜誌類別
+        span(v-else-if="this.genre === 3").font-weight-bold.title 教科書類別
+        span(v-else).font-weight-bold.title 其他
+  hr
+  b-row(align-v="stretch").mt-5
+    b-col.book_img(cols="12" md="4").mb-4.pr-md-2.pr-lg-1
+      img.w-100(:src="image")
+    b-col(cols="12"  md="8").pl-lg-5.pl-md-4
+      h1.text-left.mt-4.mb-4
+        span(style="color: #495580").font-weight-bold {{ name }}
+      h2.text-left.mb-4
+        span(style="color: #495580").font-weight-bold 捐贈者：
+      h3.text-left.mb-4
+        span {{ from.name }}
+      h2.text-left.mb-4
+        span(style="color: #495580").font-weight-bold 說明：
+      h4.text-left.mb-4
+        textarea(disabled).w-100.p-2 {{ description }}
+  hr
+  b-row
+    b-col(cols="12").mb-4.d-flex.justify-content-end
+      b-button(href='#' variant='success' @click="addCart" v-if="this.$store.state.user.role !== 1"  style="height: 10vh; line-height: 10vh").button-size.w-50.mt-3
+        #text_cart
+        b-icon(icon="bag-check-fill")
+        |  放入購物車
 </template>
 
 <script>
